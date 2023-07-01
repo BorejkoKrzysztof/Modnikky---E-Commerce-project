df4b949f0e447aded326b19f433047954a6a8e90
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2023-07-01 21:17:29 +0200

    Create Nav for Desktop

---
+++ Frontend/modnikkyuserweb/src/Components/Header/SubComponents/DesktopNavContent/DesktopNavContent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './DesktopNavContent.module.scss'
+
+function DesktopNavContent() {
+    return (
+        <section>DesktopNavContent</section>
+    )
+}
+
+export default DesktopNavContent
