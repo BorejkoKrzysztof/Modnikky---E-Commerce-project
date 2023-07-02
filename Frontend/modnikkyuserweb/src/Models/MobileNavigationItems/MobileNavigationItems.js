b3dbe9dc2466b9325750526cf46ab935d9c91da3
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2023-07-02 22:03:22 +0200

    Create test mobile menu items

---
+++ Frontend/modnikkyuserweb/src/Models/MobileNavigationItems/MobileNavigationItems.js
@@ -0,0 +1,40 @@
+export const MobileNavigationItems = [
+    {
+        name: 'Category 1.0',
+        link: '',
+        subCategories: [
+            {
+                name: 'Category 1.1',
+                link: 'xd',
+                subCategories: []
+            },
+            {
+                name: 'Category 1.2',
+                link: '',
+                subCategories: [
+                    {
+                        name: 'Category 1.2.1',
+                        link: 'xddd',
+                        subCategories: [
+                            {
+                                name: 'Category 1.2.1.2',
+                                link: 'xddd',
+                                subCategories: []
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Category 1.2.2',
+                        link: '',
+                        subCategories: []
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'Category 2.0',
+        link: 'xdddd222',
+        subCategories: []
+    }
+]
